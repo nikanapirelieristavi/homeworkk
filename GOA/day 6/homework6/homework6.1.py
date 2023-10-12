4e1985aebcd7ab4e04c82a6405dad43cec087c3b
nikanapirelieristavi <143445022+nikanapirelieristavi@users.noreply.github.com>
2023-10-12 18:25:16 +0400

    Add files via upload

---
+++ GOA/day 6/homework6/homework6.1.py
@@ -0,0 +1,19 @@
+#შექმენით თქვენი daily routine-ისგან ციკლი
+day = 1
+while day <= 31:
+    print("day ", day)
+    day = day + 1
+    print("wake up")
+    print("go to school")
+    print("work out")
+    print("study")
+    print("sleep")
+    if day <= 31:
+        print("day ", day)
+        print("wake up")
+        print("go to school")
+        print("rest")
+        print("study")
+        print("sleep")
+
+
---
+++ GOA/day 6/homework6/homework6.py
@@ -0,0 +1,11 @@
+#დავპრინტოთ 1-30ამდე რიცხვები, while loop-ის გამოყენებით, კონსოლში ყოველი რიცხვის შემდეგ დაეწეროს ლუწია თუ კენტი.
+number= 1
+
+while number<=30:
+    print("the number is odd", number)
+    number = number + 1
+    print("the number is even", number)
+    number = number + 1
+
+
+    
