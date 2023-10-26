fbd2614ddacb1b527e5596f859815b1b88abefe0
nikanapirelieristavi <143445022+nikanapirelieristavi@users.noreply.github.com>
2023-10-26 22:29:02 +0400

    Add files via upload

---
+++ GOA/day 9/classwork9/classwork9.py
@@ -0,0 +1,22 @@
+#სასტუმროში შემოდის 4 ბიჭი, და გვინდა ოთხივეს დავუპრინტოთ მოგესალმებით, დღეში სამჯერ
+
+def bichebtan_misalmeba():
+    print("hello gio")
+    print("hello nika")
+    print("hello tornike")
+    print("hello luka")
+
+print("dila")
+bichebtan_misalmeba()
+print("shuadge")
+bichebtan_misalmeba()
+print("sagamo")
+bichebtan_misalmeba()
+
+def misalmeba(saxeli):
+    print("mogesalmebi " + saxeli)
+
+misalmeba("nika")
+misalmeba("luka")
+misalmeba("gabriel")
+misalmeba("sandro")
