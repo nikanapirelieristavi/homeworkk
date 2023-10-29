1c95aa264f5483b452707659a48f7af8cc561a5e
nikanapirelieristavi <143445022+nikanapirelieristavi@users.noreply.github.com>
2023-10-29 22:13:15 +0400

    Add files via upload

---
+++ GOA/day 10/classwork 10/classwork10.py
@@ -0,0 +1,31 @@
+family = ["deda", "mama"]
+family.insert(1, "me") #insertis komandshi 1 agnishnavs adgils imis risac vsvavt da mere vsvavt nebismier sityvas an ricxvs
+family.append("sayvareli ", "adamiani") #appendi sul boloshi svavs siashi ricxvs an sityvas
+#kide aris index komandi magalitad iwereba family.index(me) da agnishnavs raime sityvis an ricxvis adgils siashi
+
+letters = ["a, b, c"]
+letters += [str(input())]
+print(letters)
+#letters+ aris damatebiti listi romelic emateba pirvel lists
+
+
+
+print(min()) #maqsimaluri ricxvi siidan
+print(max()) #minimaluri ricxvi siidan
+
+
+
+x = [2, 4, 6, 7, 9, 20]
+
+print(x.count(2)) #itvlis siashi ramdenjeracaaa elementi mocemuli
+
+x.remove(2) # shlis konkretul elements siashi
+print(x)
+
+x.reverse #abrunebs sias ukugma
+print(x)
+
+nums = [4, 5, 6]
+msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2]) #formats gamoaqvs ricxvebi siashi adgilis mixedvit
+print(msg)   #   4   5   6
+
---
+++ GOA/day 10/homework 10/homework10.py
@@ -0,0 +1,12 @@
+#family = ["tamuna", "saba", "mate"]
+#full_sentence = "My moms name is: {}, My brothers name is: {}, My name is: {}".format(family[0],family[1],family[2])
+#print(full_sentence)  დაემატოს ასაკები და დაიპრინტოს თითოეული რამდენი წლის 
+#არის ერთ გრძელ წინადადებად ხოლო მეორე დაპრინტვაზე დაიპრინტოს რამდენი წლის გახდნენ 10 წელში
+
+family = ["nika", "levani", "nino"]
+age = [15, 45, 40]
+full_sentence = "my mom's name is {}, she is {}, my dad's name is {}, he is {}, my name is {}, I am {} years old".format(family[2],age[2],family[1],age[1],family[0],age[0])
+print(full_sentence)
+age2 =[25, 55, 50]
+full_sentence2 = "my mom's name is {}, she is {}, my dad's name is {}, he is {}, my name is {}, I am {} years old".format(family[2],age2[2],family[1],age2[1],family[0],age2[0])
+print(full_sentence2)
