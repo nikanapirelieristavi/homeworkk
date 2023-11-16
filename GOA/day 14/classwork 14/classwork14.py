566c72a8f7ce06fef7a9a6fcb11c0709bf2d50a0
nikanapirelieristavi <143445022+nikanapirelieristavi@users.noreply.github.com>
2023-11-16 17:28:06 +0400

    Add files via upload

--- GOA/day 14/classwork 14/classwork14.py
+++ GOA/day 14/classwork 14/classwork14.py
@@ -1,10 +1,13 @@
-def no_space(x):
-    return x.replace(' ','')
-# es funqcia anacvlebs gamotovebul adgilebs carieli adgilit
+# def no_space(x):
+#     return x.replace(' ','')
+# # es funqcia anacvlebs gamotovebul adgilebs carieli adgilit
 
-def count_sheeps(sheep):
-    amount_of_sheep = 0
-    for element in sheep:
-        if element == True:
-            amount_of_sheep += 1
-    return amount_of_sheep
+# def count_sheeps(sheep):
+#     amount_of_sheep = 0
+#     for element in sheep:
+#         if element == True:
+#             amount_of_sheep += 1
+#     return amount_of_sheep
+
+list = [2, 3, 4]
+print(list[2])
---
+++ GOA/day 15/homework 15/homework15.py
@@ -0,0 +1,85 @@
+""""
+#1
+print
+#2
+integer
+#3
+string
+#4
+20
+#5
+answer = 42
+#6
+debugging
+#7
+underscore since python would not recognize spaces
+#8
+to know what purpose a certain line of code has
+#9
+print(input())
+#10
+--
+#11
+type is a function which identifies data types of variables
+#12
+with -> "" or str() function
+#13
+es movidzie - type casting or type conversion
+#14
+the operator that checks if two values are equal is ==
+#15
+ragaca = True and False
+print(result)
+it would print False. if there were two True values it would print True but not in this case
+#16
+True because both of the statements are correct
+#17
+it's if elif and else due to the fact that those are the only ones we've learnt so far that function based on certain conditions
+#18
+for loop can iterate over lists and strings
+#19
+while loop. as its name suggests it repeats a block of code WHILE a condition is true
+#20
+range() function primarily used in for loops is used to generate a certain set of numbers which can be iterated over
+#21
+if ??
+#22
+if something isn't valid for "if" we use "else" for these types of situations
+#23
+list is used to store information in the same way we use lists in real life
+#25
+since we start counting from zero in this instance
+list = [2, 3, 4]
+print(list[]) in square brackets we have to type 2 and it will print number four
+#24
+in ptyhon we start counting from zero, so we need to type 0 in square brackets if we want the first element of the list
+#26
+slicing allows us to slice a portion of a list or a string
+#27
+for example
+my_list = [1, 2, 3, 4, 5, 6, 7]
+sliced_list = [2:6]
+print(sliced_list)
+this would print numbers between 2 and 5 including number 5 itself
+#28
+there are three values that we use in slicing
+the first is start, then stop, then step
+
+start is the index of the first element
+
+stop is the index of the first element to exclude
+
+step is the interval between elements
+#29
+we create reusable blocks of codes with functions
+#30
+parameters
+#31
+we use upper() to convert a string to uppercase
+#32
+it's append
+#33
+the keyword is "def"
+#34
+we use "return" to return a value from a function
+"""
