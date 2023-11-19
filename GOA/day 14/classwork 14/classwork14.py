a1f79719ff1eff07a371a1df244009585f96f082
nikanapirelieristavi <143445022+nikanapirelieristavi@users.noreply.github.com>
2023-11-19 15:06:30 +0400

    Add files via upload

--- GOA/day 14/classwork 14/classwork14.py
+++ GOA/day 14/classwork 14/classwork14.py
@@ -1,13 +1,10 @@
-# def no_space(x):
-#     return x.replace(' ','')
-# # es funqcia anacvlebs gamotovebul adgilebs carieli adgilit
+def no_space(x):
+    return x.replace(' ','')
+# es funqcia anacvlebs gamotovebul adgilebs carieli adgilit
 
-# def count_sheeps(sheep):
-#     amount_of_sheep = 0
-#     for element in sheep:
-#         if element == True:
-#             amount_of_sheep += 1
-#     return amount_of_sheep
-
-list = [2, 3, 4]
-print(list[2])
+def count_sheeps(sheep):
+    amount_of_sheep = 0
+    for element in sheep:
+        if element == True:
+            amount_of_sheep += 1
+    return amount_of_sheep
